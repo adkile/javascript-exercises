942afa84cd85a3ac3dc2b8961b5a407faec9e176
adkile <ademkilinc.dev@gmail.com>
2025-06-21 19:26:46 +0200

    completed exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let newStr = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      newStr += str;
+    }
+    return newStr;
+  } else {
+    return "ERROR";
+  }
 };
+
+console.log(repeatString("Bananas", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
