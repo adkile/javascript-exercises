8e1d40aa870e90a4cc67013ff13e5386078d060c
adkile <ademkilinc.dev@gmail.com>
2025-06-22 21:02:37 +0200

    finished 2 exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let cleanedStr = str.toLowerCase().replace(/[,. !?]/gi, "");
+  let strArr = cleanedStr.split(" ").join("");
+  let reversedStr = cleanedStr.split("").reverse().join("");
 
+  if (strArr === reversedStr) {
+    return true;
+  } else return false;
 };
+
+console.log(palindromes("A nut for a jar of tuna."));
 
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacciSuite = [0, 1];
 
+function createFibonacciSuite(num) {
+  for (let i = 2; i < num; i++) {
+    fibonacciSuite.push(fibonacciSuite[i - 1] + fibonacciSuite[i - 2]);
+  }
+}
+
+const fibonacci = function (num) {
+  createFibonacciSuite(num + 1);
+  if (num < 0) {
+    return "OOPS";
+  } else return fibonacciSuite[num];
 };
+
+console.log(fibonacci(38));
 
 // Do not edit below this line
 module.exports = fibonacci;
